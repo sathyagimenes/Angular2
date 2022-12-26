3dc2241b1c2986ed1468f062991ec301a409adf6
sathyagimenes <sathya.gimenes@gmail.com>
2022-12-26 12:07:12 -0300

    adicionei formulário

--- crud/src/app/products/components/create-product/create-product.component.ts
+++ crud/src/app/products/components/create-product/create-product.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-create-product',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-product.component.css']
 })
 export class CreateProductComponent {
+  public form!: FormGroup;
+  public product!: Product;
+  public id!: string;
+
+  // constructor(private usersService: UsersService, private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.buildForm();
+    // this.getRouteParams();
+  }
+
+  // private getRouteParams() {
+  //   this.id = this.activatedRoute.snapshot.params['id'];
+  //   this.setFormValue();
+  // }
+
+  // private setFormValue() {
+  //   const filteredUser = this.usersService.getUserById(this.id);
+  //   if (filteredUser != null) {
+  //     this.form.get('documentNumber')?.disable();
+  //     const submitBtn = window.document.getElementsByName('submitBtn');
+  //     submitBtn.item(0).innerText = 'Editar';
+  //     const titulo = window.document.getElementsByName('titulo');
+  //     titulo.item(0).innerText = 'Edição de cadastro';
+  //   }
+  //   this.form.patchValue(filteredUser);
+  // }
+
+  private buildForm(): void {
+    this.form = new FormGroup({
+      id: new FormControl(),
+      name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      description: new FormControl(null, [Validators.required]),
+      expirationDate: new FormControl(null, [Validators.required]),
+      price: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.pattern('^[0-9]+,[0-9]{2}')])
+    });
+  }
+
+  public onSubmit(): void {
+    this.product = this.form.getRawValue();
+    // const filteredUser = this.usersService.getUserById(this.id);
+    // if (filteredUser != null) {
+    //   this.usersService.updateUser(this.user);
+    // }
+    // else {
+      // this.usersService.saveUser(this.user);
+    // }
+    this.form.reset();
+    // this.router.navigate(['/users']);
+  }
+
 
 }
